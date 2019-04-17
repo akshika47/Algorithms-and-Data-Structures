48aeb3fb48aee24f8406494d1dcc61cb52f83efd
Akshika47 <akshikaakalanka@gmail.com>
2019-04-17 17:59:00 +0100

    added insertion sort

---
+++ Algorithms/Sorting/Insertion_Sort.py
@@ -0,0 +1,16 @@
+from array import array
+import time
+
+ticks1 = time.time()
+A = [5,2,4,6,1,3]
+
+for j in range(1, len(A)):
+	key = A[j]
+	i = j-1
+	while (i>-1 and A[i] > key):
+		A[i+1] = A[i]
+		i = i-1
+	A[i+1] = key
+ticks2 = time.time();
+print(A)
+print("Time spent", ticks2-ticks1)
